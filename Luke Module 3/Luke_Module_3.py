9dc4321229210fd309275a891b42a2a36bdb1063
luke edney <edneypompey@hotmail.co.uk>
2021-09-21 17:58:54 +0100

    Add project files.

---
+++ Luke Module 3/Luke_Module_3.py
@@ -0,0 +1,8 @@
+###### Luke Edney Tues 21 Sep 
+
+###### 3.1.1.4
+# write a simple two-line program that takes the parameter n as input, which is an integer, 
+# and prints False if n is less than 100, and True if n is greater than or equal to 100
+
+n = int(input("Type a number")) # allows the input after the string
+print(n >= 100) #true/false reply if n greater or equal to 100
