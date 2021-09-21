8d5426c211f02e09a2b1957b4f386bdcf5d0e01b
luke edney <edneypompey@hotmail.co.uk>
2021-09-21 19:37:56 +0100

    lab 3

--- Luke Module 3/Luke_Module_3.py
+++ Luke Module 3/Luke_Module_3.py
@@ -1,8 +1,8 @@
 ###### Luke Edney Tues 21 Sep 
 
 ###### 3.1.1.4
-# write a simple two-line program that takes the parameter n as input, which is an integer, 
-# and prints False if n is less than 100, and True if n is greater than or equal to 100
+#write a simple two-line program that takes the parameter n as input, which is an integer, 
+#and prints False if n is less than 100, and True if n is greater than or equal to 100
 
 n = int(input("Type a number: ")) # allows the input after the string
 print(n >= 100) #true/false reply if n greater or equal to 100
@@ -21,3 +21,28 @@ elif flower == "spathiphyllum": #if its does not use the correct case the print 
     print("No, I want a big Spathiphyllum!")
 else:
     print("Spathiphyllum! Not ", flower, "!", sep="") # otherwise print this
+
+####3.1.1.11
+
+#Your task is to write a tax calculator
+#It should accept one floating-point value: the income
+#Next it should print the calculated tax, rounded to full thalers. 
+#There's a function named round() which will do the rounding for you - you'll find it in the skeleton code in the editor.
+#If the calculated tax is less than zero, it only means no tax at all 
+#(the tax is equal to zero). Take this into consideration during your calculations.
+
+income = float(input("Enter the annual income: "))
+
+if income <= 85528: #lower value of tax
+    tax = income * .18 #finds 18 percent of the income (tax)
+    tax = tax - 556.02 #minus the tax relief value of 556.02
+else: 
+    tax = 14839.02 #higher value of tax
+    extra = income - 85528
+    extra_tax = extra * .32 #finds 32 percent of the income (tax)
+    tax = tax + extra_tax # add extra tex to tax
+if tax < 0: # if tax is less than 0 not tax to be paid.
+    tax =0
+
+tax = round(tax, 0)
+print("The tax is:", tax, "thalers")
