033793572f1b803594a64a9396f0ddb2d009d586
luke edney <edneypompey@hotmail.co.uk>
2021-09-23 13:05:37 +0100

    updated

--- Luke Module 3/Luke_Module_3.py
+++ Luke Module 3/Luke_Module_3.py
@@ -250,3 +250,29 @@ hat_list[2] = user_input # Copying value of the user input into indxex 2 (middle
 del hat_list[4] # Removing index 4 element from the list.
 
 print("List Length: ", len(hat_list)) #print the length of the lists' elements.
+
+#### 3.4.1.13
+
+# step 1: create an empty list named beatles;
+beatles = []
+print("Step 1:", beatles)
+
+#step 2: use the append() method to add the following members of the band to the list: 
+#John Lennon, Paul McCartney, and George Harrison;
+beatles.append('John Lennon',)# add John to the list
+beatles.append('Paul McCartney') # add Pual to the list
+beatles.append('George Harrison') # add george to the list
+print("Step 2:", beatles)
+
+# step 3: use the for loop and the append() method to prompt the user 
+# to add the following members of the band to the list: Stu Sutcliffe, and Pete Best
+
+##### DID NOT UNDERSTAND HOW TO DO STEP 3, (struggled with the 'for loop' user input)
+
+# step 4: use the del instruction to remove Stu Sutcliffe and Pete Best from the list;
+del beatles[1]  # Removing Paul from the list (as couldnt append stu via for loop)
+print("Step 4:", beatles)
+
+#step 5: use the insert() method to add Ringo Starr to the beginning of the list.
+beatles.insert(0, 'Ringo Starr') # add the strinf Ringo Starr into index 0
+print("Step 5:", beatles)
